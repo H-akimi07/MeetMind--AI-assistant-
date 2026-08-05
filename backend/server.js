8e9361a525e8e6d6d21cd0077125574f9ef23716
https://github.com/H-akimi07 <hakimisameera56@gmail.com>
2026-08-05 13:27:34 +0430

    I make some changes on its port

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,25 @@
 require("dotenv").config();
 
-
 const meetingRoutes = require("./routes/meetingRoutes.js");
 const userRoutes = require("./routes/userRoutes.js");
 const authRoutes = require("./routes/authRoutes.js");
 const connectDB = require("./config/db.js");
 const express = require("express");
 const cors = require("cors");
-const contactRoutes=require("./routes/contactRoutes");
+const contactRoutes = require("./routes/contactRoutes");
 const path = require("path");
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
 
-app.use(
-  "/uploads",
-  express.static(path.join(__dirname, "uploads"))
-);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // Connect Database
 connectDB();
-
 
 // Middleware
 app.use(cors());
@@ -30,25 +28,21 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/meetings", meetingRoutes);
 
-
-
 // Test Route
-app.get("/", (req,res)=>{
-    res.send("MeetMind Backend is running!");
+app.get("/", (req, res) => {
+  res.send("MeetMind Backend is running!");
 });
 
-
-app.get("/api/test",(req,res)=>{
-    res.json({
-        message:"Hello from MeetMind Backend!"
-    });
+app.get("/api/test", (req, res) => {
+  res.json({
+    message: "Hello from MeetMind Backend!",
+  });
 });
-
 
 // Start Server
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 //Contact
-app.use("/api/contact",contactRoutes);
+app.use("/api/contact", contactRoutes);
