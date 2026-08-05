536fcb20650432d60381f64c76c7ea5506f62e0a
https://github.com/H-akimi07 <hakimisameera56@gmail.com>
2026-08-05 13:29:37 +0430

    upgrade te sever file

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,35 @@
 require("dotenv").config();
 
-const meetingRoutes = require("./routes/meetingRoutes.js");
-const userRoutes = require("./routes/userRoutes.js");
-const authRoutes = require("./routes/authRoutes.js");
-const connectDB = require("./config/db.js");
 const express = require("express");
 const cors = require("cors");
-const contactRoutes = require("./routes/contactRoutes");
 const path = require("path");
+
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const meetingRoutes = require("./routes/meetingRoutes");
+const contactRoutes = require("./routes/contactRoutes");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
-
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // Connect Database
 connectDB();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Static uploads
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/meetings", meetingRoutes);
+app.use("/api/contact", contactRoutes);
 
-// Test Route
+// Test routes
 app.get("/", (req, res) => {
   res.send("MeetMind Backend is running!");
 });
@@ -39,10 +40,9 @@ app.get("/api/test", (req, res) => {
   });
 });
 
-// Start Server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Start server
+const PORT = process.env.PORT || 5000;
 
-//Contact
-app.use("/api/contact", contactRoutes);
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
