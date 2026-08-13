cc91508d9503993921c26cea6a7adf4889dcd65e
https://github.com/H-akimi07 <hakimisameera56@gmail.com>
2026-08-13 21:08:17 +0430

    contact debugging

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -22,6 +22,8 @@ const sendMessage = async (req, res) => {
       port: 465,
       secure: true,
 
+      family: 4,
+
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
