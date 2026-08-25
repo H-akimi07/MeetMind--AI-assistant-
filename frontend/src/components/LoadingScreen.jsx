99f3def45a149cc224f03235214494235a94abec
https://github.com/H-akimi07 <hakimisameera56@gmail.com>
2026-08-25 16:22:55 +0430

    loading design added

--- frontend/src/components/LoadingScreen.jsx
+++ frontend/src/components/LoadingScreen.jsx
@@ -1,15 +1,33 @@
 import "./LoadingScreen.css";
-import { FiLoader } from "react-icons/fi";
+import { FiLoader, FiCpu } from "react-icons/fi";
 
 function LoadingScreen({ title }) {
   return (
     <div className="loading-screen">
+      <div className="loading-glow loading-glow-one"></div>
+      <div className="loading-glow loading-glow-two"></div>
+
       <div className="loading-box">
-        <FiLoader className="loading-icon" />
+        <div className="loading-icon-wrapper">
+          <FiCpu className="loading-icon" />
+        </div>
 
-        <h2>Loading {title}...</h2>
+        <div className="loading-spinner-ring"></div>
 
-        <p>Please wait a moment.</p>
+        <h2>
+          Loading <span>{title}</span>
+        </h2>
+
+        <p>Please wait a moment while MeetMind prepares everything.</p>
+
+        <div className="loading-progress">
+          <span></span>
+        </div>
+
+        <div className="loading-status">
+          <FiLoader />
+          <span>Processing securely...</span>
+        </div>
       </div>
     </div>
   );
